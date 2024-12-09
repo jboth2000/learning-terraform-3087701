390c1df8acc37011844fa2f8f85b696b661a4b82
jboth2000 <108500655+jboth2000@users.noreply.github.com>
2024-12-09 13:58:27 -0700

    update

--- main.tf
+++ main.tf
@@ -34,21 +34,19 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
-  # insert the 1 required variable here
+  version = "6.5.2"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id               = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
